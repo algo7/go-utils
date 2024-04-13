a10d9eaacaeef135d6936352c986bee1e5df4147
algo7 <11154774+algo7@users.noreply.github.com>
2024-04-13 20:24:37 +0200

    add comments about the strictunmarhsalJSON func

--- encoding/json/json.go
+++ encoding/json/json.go
@@ -9,22 +9,25 @@ import (
 
 // StrictUnMarshalJSON is a function that unmarshals JSON data into a target interface
 // It will return an error if there are any unknown fields in the JSON data and if there is any remaining data after decoding
+// This function is useful for preventing configuration errors or security issues by ensuring the input JSON fully conforms to expected formats
 // Example:
 // data := []byte(`{"name": "John", "age": 30}`)
 //
 //	var person struct {
-//	    Name string `json:"name"`
+//	  Name string `json:"name"`
 //	}
 //
-// err := StrictUnMarshalJSON(data, &person)
+//	err := StrictUnMarshalJSON(data, &person)
 //
 //	if err != nil {
-//	    fmt.Println(err)
+//	  fmt.Println(err)
 //	}
+//	fmt.Println(person.Name)
 func StrictUnMarshalJSON(data []byte, target interface{}) error {
 
 	// Create a new JSON decoder and disallow unknown fields
 	decoder := json.NewDecoder(bytes.NewReader(data))
+	// Prevents unmarshaling of unknown fields
 	decoder.DisallowUnknownFields()
 
 	// Decode the JSON data into the target interface
